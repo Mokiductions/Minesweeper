2b659701af004d4dcbcde42d7b88103bacbd1578
Miquel Ginés <ginesborrasm@gmail.com>
2017-01-03 12:42:25 +0100

    Implementado método de introducción del tamaño del lado y de la cantidad de bombas por el usuario.

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -3,6 +3,11 @@ import java.awt.*;
 
 public class Minesweeper extends JFrame {
 
+    private final int width = 200;
+    private final int height = 200;
+    private int cells = 10;
+    private int bombs = 10;
+
     public Minesweeper() {
         super("Minesweeper!");
         initGUI();
@@ -23,13 +28,53 @@ public class Minesweeper extends JFrame {
     private void initGUI() {
         Container c = getContentPane();
         c.setLayout(new BorderLayout(0, 0));
-        int width = 200;
-        int height = 200;
-        int cells = 10;
-        int bombs = 10;
+        askForOptions();
         Game game = new Game(width, height, cells, bombs);
         c.add(game, BorderLayout.CENTER);
     } // End of initGUI()
+
+    /**
+     * Pide las opciones del juego al usuario, lado del tablero, bombas.
+     */
+    private void askForOptions() {
+        // Dimension del tablero (lado, variable cells)
+        cells = askForCells();
+
+        // Cantidad de bombas
+        bombs = askForBombs();
+    } // End of askForOptions()
+
+    /**
+     * Pide al usuario cuantas celdas quiere por lado del tablero.
+     *
+     * @return Integer - Numero introducido por el usuario
+     */
+    private int askForCells() {
+        int i = 10;
+        String s = JOptionPane.showInputDialog("Numero de celdas (lado del cuadrado)");
+        try {
+            i = Integer.parseInt(s);
+        } catch (Exception e) {
+            askForCells();
+        }
+        return i;
+    } // End of askForCells()
+
+    /**
+     * Pide al usuario cuantas bombas quiere en el tablero.
+     *
+     * @return Integer - Numero introducido por el usuario
+     */
+    private int askForBombs() {
+        int i = 10;
+        String s = JOptionPane.showInputDialog("Numero de bombas");
+        try {
+            i = Integer.parseInt(s);
+        } catch (Exception e) {
+            askForOptions();
+        }
+        return i;
+    } // End of askForBombs()
 
     /**
      * Metodo main, crea el primer hilo de ejecucion del juego.
