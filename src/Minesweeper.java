b9c418fb9dac80761e2bc7337a2173ed9199a463
Miquel Ginés <ginesborrasm@gmail.com>
2017-01-03 12:47:27 +0100

    Implementado método de introducción del tamaño del lado y de la cantidad de bombas por el usuario.

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -71,7 +71,7 @@ public class Minesweeper extends JFrame {
         try {
             i = Integer.parseInt(s);
         } catch (Exception e) {
-            askForOptions();
+            askForBombs();
         }
         return i;
     } // End of askForBombs()
